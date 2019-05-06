9cd8b5fd6f9ebc3e7fb3aa77c6ba504de0e7a3d1
dario platania <dariopl91@gmail.com>
2019-05-06 11:05:23 +0200

    modified path application

--- serverJS/server.js
+++ serverJS/server.js
@@ -2,7 +2,7 @@ var connect     = require('connect');
 var serveStatic = require('serve-static');
 var server = connect();
 
-server.use(serveStatic('../../angularJS/esercizi/es3_myAmazon'));
+server.use(serveStatic('AngularJS-example'));
 
-console.log('Server On');   
+console.log('Server On');
 server.listen(5000);
